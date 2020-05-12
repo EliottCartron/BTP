8bac2e1e670c6c03cf7727d5376444bc2d2936f6
Brice <brice.lohrer@gmail.com>
2020-05-12 15:10:01 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/btp/model/Views.java
@@ -0,0 +1,12 @@
+package btp.model;
+
+public class Views {
+	
+	public static class ViewCommon {}
+	
+	public static class ViewAction extends ViewCommon {}
+	
+	public static class ViewAppelOffre extends ViewCommon {}
+	
+	public static class ViewFacture extends ViewCommon {}
+}
